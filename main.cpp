147262596e29013512aa9a4f5b7453bcae5f1c11
Patrisha-de-Boon <32069658+Patrisha-de-Boon@users.noreply.github.com>
2018-06-13 12:54:43 -0600

    Started working on main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
 
+// This is my attempt at building a basic chat bot by skratch
+
+#include <iostream>
+using namespace std;
+
+cout << "Hey there! If you don't mind me asking, what is your name?" << endl;
+string response;
+cin >> response;
+
+string currName = "90195=";
+string space = " ";
+if (!(response.find(space) != std::string::npos)) {
+    currName = response;
+}
+else {
+    //querry api.ai or whatever I use
+    break;
+}
+
+if (currName != "90195=" && currName != shared.username && shared.username != "90195=") {
+    cout << "Sorry, I thought your name was " << shared.username << ". Which name do you prefer I use for you?" << endl;
+}
+
